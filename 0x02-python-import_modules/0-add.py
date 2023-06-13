bdf6163643f8dc88f27d54114499f8bd98556a47
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-06-13 20:41:49 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+if __name__ != "__main__":
+    exit()
 from add_0 import add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print(f"{a:d} + {b:d} = {add(a, b):d}")
