44d7d6b44d048b53ec6688a7f4f0e3dd183aaf93
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-06-14 18:53:34 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,4 +2,11 @@
 if __name__ != "__main__":
     exit()
 from sys import argv
-print("{}".format(argv))
+if len(argv) == 1:
+    print("0 arguments.")
+elif len(argv) == 2:
+    print("1 argument:")
+else:
+    print(f"{len(argv)-1} arguments:")
+for i in range(1, len(argv)):
+    print(f"{i}: {argv[i]}")
