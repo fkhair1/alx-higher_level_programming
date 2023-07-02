42980c01fa4d1554e06cd6706454ceeea1a8601c
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-02 17:49:30 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represents an empty square."""
-    def __init__(self,size =0):
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
