f4e846082960efc9dacfc73170efdb3785fcb900
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-02 20:37:18 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,7 +9,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if (len(position) != 2 or type(position[0]) != int or type(position[0]) != int or
+        if (len(position) != 2 or type(position[0]) != int or
+            type(position[1]) != int or
                 position[0] < 0 or position[1] < 0):
             raise ValueError("position must be a tuple of 2 positive integers")
         self.__size = size
