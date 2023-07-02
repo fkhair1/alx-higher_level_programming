20922c02cde7db3ae972a9340664acf5660af28f
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-02 20:39:01 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,12 +30,12 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @property
     def position(self):
         return self.__position
     
-    @position.setter    
+    @position.setter
     def position(self, value):
         if len(value) != 2 or value[0] < 0 or value[1] < 0:
             raise ValueError("position must be a tuple of 2 positive integers")
