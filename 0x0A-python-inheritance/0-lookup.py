ce5f48c1d7af11500a433df02da4b56cd570962a
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-10 19:41:41 +0100

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Contains the lookup function"""
+
+
 def lookup(obj):
-    """ jhkjh kjhkj """
-    return list(dir(obj))
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
