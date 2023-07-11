2ead2aba171746471486d949b1f7d9bb74dcd2c9
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-11 21:50:36 +0100

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,7 @@
-***
-***
+#!/usr/bin/python3
+''' 4-inherits_from.py '''
+
+
+def inherits_from(obj, a_class):
+    ''' inherits_from '''
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
