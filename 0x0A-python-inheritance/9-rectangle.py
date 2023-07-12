845ee499502c7360a07e1df60438a6616da76792
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-12 15:42:44 +0100

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,2 +1,20 @@
-***
-***
+#!/usr/bin/python3
+"""9-rectangle.py"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle"""
+
+    def __init__(self, width, height):
+        self.__width = self.integer_validator('width', width)
+        self.__height = self.integer_validator('height', height)
+ 
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+        
