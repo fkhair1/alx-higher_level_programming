cd078780900cf37710da4d721f3debc1a7d9c5f5
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-13 08:53:37 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,2 +1,20 @@
-***
-***
+#!/usr/bin/python3
+"""10-square.py"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
