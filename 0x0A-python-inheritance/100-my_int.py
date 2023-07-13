71596fd2704c08acc71bf131f3d46548df4f8491
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-07-13 08:56:24 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,2 +1,12 @@
-***
-***
+#!/usr/bin/python3
+"""100-my_int.py"""
+
+
+class MyInt(int):
+    """class MyInt"""
+
+    def __eq__(self, other):
+        return not (int(self) == other)
+
+    def __ne__(self, other):
+        return not (int(self) != other)
