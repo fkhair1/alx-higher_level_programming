40ecee1f5130c6246d11732080a3cdfe1358e744
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-11-18 22:14:44 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-if (ParsInt(args[2])){
-  console.log('My number: ' + ParsInt(args[2]));
+if (ParseInt(args[2])){
+  console.log('My number: ' + ParseInt(args[2]));
 } else{
   console.log('Not a number');
 }
