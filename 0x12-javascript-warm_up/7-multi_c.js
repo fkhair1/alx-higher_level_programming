796fa11dd4af6a91d7b32a8bd1bff10803b72891
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-11-19 21:13:47 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
+if (parseInt.args[2]){
+  console.log('Missing number of occurrences');
+}
 for (const i = 0; i < args.length; i++) {
 console.log(args[i]);
 }
