658d71ecaf793fd5f4c2c0df9e18e855a05647d8
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-11-19 22:06:41 +0100

    Update 8-test.js

--- 0x12-javascript-warm_up/8-test.js
+++ 0x12-javascript-warm_up/8-test.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const args = process.argv;
+if (args.length > 2) {
+  for (let i = 0; i < parseInt(args[2]); i++) {
+    for (let j = 0; j < parseInt(args[2]); j++) {
+    console.log('X');
+  }
+  }
+} else {
+  console.log('Missing size');
+}
