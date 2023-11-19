53f076b7e1f2fd86cf0f8b32abea1bbc502a4a24
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-11-19 21:28:04 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args > 2){
-  or (const i = 0; i < args[2]; i++) {
+if (args > 2) {
+  for (const i = 0; i < args[2]; i++) {
   console.log('C is fun');
-} else{
+  }
+} else {
   console.log('Missing number of occurrences');
 }
