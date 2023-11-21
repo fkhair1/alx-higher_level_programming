97c92300b63e15cc1b473f92e93a3f28852255c8
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-11-21 23:24:03 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const args = process.argv;
 function factorial (a) {
- if (parseInt(a)) {
   let n = 1;
-  for (let i = 0; i <= parseInt(a); i++) {
-    console.log(n = n * i);
-    } 
- } else {
-    console.log(1);
+  if (parseInt(a)) {
+    for (let i = 1; i <= parseInt(a); i++) {
+      n = n * i;
+    }
   }
+   console.log(n);
 }
 factorial (args[2]);
