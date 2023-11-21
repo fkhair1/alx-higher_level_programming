488fe55de182e9e70d8acf5aec2d4c4865731112
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-11-21 21:40:02 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a+b;
-}
+function add (a, b) {
+  console.log(a+b);
+};
 const args = process.argv;
-if (parseInt(args[2]) && parseInt(args[3])) {
-  console.log(add((parseInt(args[2])), (parseInt(args[3]))));
-}
+console.log(parseInt(args[2]), parseInt(args[3]));
