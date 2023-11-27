bec68cf4844ffb77c497ba77869bd98f9a36cb90
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-11-27 11:05:18 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,24 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
+const args = process.argv;
 
-myObject.incr = function () {
-  this.value += 1;
-};
-
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+if (args.length <= 3) {
+  console.log(0);
+} else {
+  let max1 = parseInt(args[2]);
+  let max2 = parseInt(args[3]);
+  if (parseInt(args[3]) > parseInt(args[2])) {
+    max1 = parseInt(args[3]);
+    max2 = parseInt(args[2]);
+  }
+  for (let i = 4; i < args.length; i++) {
+    if (max2 < parseInt(args[i])) {
+      if (max1 < parseInt(args[i])) {
+        max2 = max1;
+        max1 = parseInt(args[i]);
+      } else {
+        max2 = parseInt(args[i]);
+      }
+    }
+  }
+  console.log(max2);
+}
