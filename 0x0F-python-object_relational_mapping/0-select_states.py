4812a01b51782249343711dadad3d4a36c8d3cc9
fkhair1 <66148243+fkhair1@users.noreply.github.com>
2023-12-04 15:19:11 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,25 @@
+#!/usr/bin/python3
+""" 0x0F-python-object_relational_mapping """
 import MySQLdb
-import sys 
-# Open database connection 
-username = sys.argv[1]
-password = sys.argv[2]
-namebdd = sys.argv[3]
-db = mysql.connect(host="localhost",user=username,password=password,database=namebdd) 
-cursor = db.cursor()
-sql = "SELECT * FROM states ORDER BY states.id ASC"
-cursor.excute(sql)
-# disconnect from server 
-db.close() 
+import sys
+
+
+if __name__ == "__main__":
+    MY_HOST = "localhost"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+
+    db = MySQLdb.connect(host=MY_HOST,
+                         user=username,
+                         passwd=password,
+                         db=name)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    for row in cur.fetchall():
+        print(row)
+
+    db.close()
